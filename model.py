345ed574f43b9c8f737c12b93350feba1d636ce6
sherwynds <w7s1b@ugrad.cs.ubc.ca>
2018-09-05 17:08:15 -0700

    Implementation of KNN with uniform weight distribution

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 import numpy as np
-
+from sklearn.neighbors import KNeighborsClassifier
 
 class CreditModel:
-    def __init__(self):
+    def __init__(self, n=6):
         """
         Instantiates the model object, creating class variables if needed.
         """
 
         # TODO: Initialize your model object.
-        pass
+        self.knn = KNeighborsClassifier(n_neighbors=n, weights='uniform')
 
     def fit(self, X_train, y_train):
         """
@@ -19,7 +19,7 @@ class CreditModel:
         """
 
         # TODO: Fit your model based on the given X and y.
-        pass
+        self.knn.fit(X_train, y_train)
 
     def predict(self, X_test):
         """
@@ -30,4 +30,5 @@ class CreditModel:
         """
 
         # TODO: Predict on `X_test` based on what you learned in the fit phase.
-        return np.random.randint(2, size=len(X_test))
+        predictions = self.knn.predict(X_test)
+        return predictions
